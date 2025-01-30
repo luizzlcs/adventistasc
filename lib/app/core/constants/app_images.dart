5d878cc4f1dc0d83584909cd59a5e2a85dd5ded1
Luiz Carlos <luizzlcs@gmail.com>
2025-01-29 21:26:40 -0300

    Contantes de imagens

---
+++ lib/app/core/constants/app_images.dart
@@ -0,0 +1,16 @@
+abstract class AppImages {
+  static const estudoBiblico = 'assets/images/estudoBiblico.jpeg';
+  static const youtube = 'assets/images/youtube.jpg';
+  static const logoIasd = 'assets/images/LogoIgreja.png';
+  static const avatar = 'assets/images/avatar.png';
+  static const logoChurch = 'assets/images/logoCherch.png';
+  static const map = 'assets/images/map.png';
+  static const instagram = 'assets/images/instagram.png';
+  static const sevenMe = 'assets/images/7me.png';
+  static const facebook = 'assets/images/facebook.png';
+  static const acampamento = 'assets/images/acampamento.jpg';
+  static const siteLogoAdv = 'assets/images/siteLogoAdv.jpg';
+  static const logoDbv = 'assets/images/logoDbv.png';
+  static const logoAvt = 'assets/images/logoAvt.png';
+  static const logoApostolos = 'assets/images/logoApostolos.png';
+}
