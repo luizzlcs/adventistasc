6f7c876d0a3a42451ca202f68e7eb6f252f34df3
Luiz Carlos <luizzlcs@gmail.com>
2025-01-29 21:27:05 -0300

    Contantes de links

---
+++ lib/app/core/constants/url_links.dart
@@ -0,0 +1,31 @@
+
+abstract class UrlLinks{
+
+static const String youtube = 'https://www.youtube.com/@adventistasc';
+  static const String  instagram = 'https://www.instagram.com/adventistasc';
+  static const  String  retiroEspiritual = 'https://bit.ly/iasd_stac';
+  static const  String  facebook = 'https://www.facebook.com/iasdstacatarina';
+  static const  String  doacao = 'https://bit.ly/AdventistaSC';
+  static const  String  estudoBiblico = 'https://docs.google.com/forms/d/e/1FAIpQLSehl8KV8dYuj5gMY8snG_-Ip2mkhoEYkAndUH2iEsCOlgvMew/viewform';
+  static const  String  localizacao = 'https://www.google.com/maps/place/Igreja+Adventista+Do+S%C3%A9timo+Dia+%7C-+Conjunto+Santa+Catarina+-+Av.+Guadalupe,+2339+-+Potengi,+Natal+-+RN,+59112-220/data=!4m2!3m1!1s0x7b3abb2f4d0ef17:0xac87e1b8d3c44b4?utm_source=mstt_1&entry=gps&g_ep=CAESBzExLjMwLjMYACD___________8BKgA%3D';
+  static const  String  clubeDesbravadores = 'https://www.instagram.com/desbravantares';
+  static const  String  clubeAventureiros = 'https://www.instagram.com/estrelasamanha';
+  static const  String  bandaApostolos = 'https://www.instagram.com/ministerio.apostolos';
+  static const  String  siteAdventista = 'https://www.adventistas.org/pt/';
+
+  // MENSAGEM
+
+static const String youtubeMsg = """🌟 Acompanhe igreja e saiba mais sobre nossa missão. Clique aqui: $youtube 💫""";
+  static const String  instagramMsg = """🌟 Acompanhe igreja e saiba mais sobre nossa missão. Clique aqui: $instagram 💫""";
+  static const  String  retiroEspiritualMsg = """🏕️ Nosso Acampamento de Verão está chegando! Bora garantir a tua vaga nesse Retiro Espiritual que promete ser incrível? Clica no link e já faz tua inscrição: $retiroEspiritual 💫""";
+  static const  String  facebookMsg = """🌟 Acompanhe igreja e saiba mais sobre nossa missão. Clique aqui: https://bit.ly/iasd_stac 💫""";
+  static const  String  doacaoMsg = '''🌟 "O Senhor é a minha força e o meu escudo; nele confiou o meu coração, e fui ajudado. (Salmos 28:7)" Para contibuir de forma fácil e rápida clica no link: $doacao 💫''';
+  static const  String  estudoBiblicoMsg = """🌟 Vamos estudar a Bíblia?. Clique aqui: https://bit.ly/iasd_stac 💫""";
+  static const  String  localizacaoMsg = """🌟 Para saber onde fica nossa igreja clique aqui: $localizacao 💫""";
+  static const  String  clubeDesbravadoresMsg = """🌟 Conheça o nosso clube de Desbravadores: $clubeDesbravadores 💫""";
+  static const  String  clubeAventureirosMsg = """🌟 Conheça o nosso clube de Aventureiros: $clubeAventureiros 💫""";
+  static const  String  bandaApostolosMsg = """🌟 Conheça a banda apóstolos e suas atividades: $bandaApostolos 💫""";
+  static const  String  siteAdventistaMsg = """🌟 Acesso o site ofial dos Adventista do 7º Dia e conheça mais sobre nossa igreja: $siteAdventista 💫""";
+  
+
+}
