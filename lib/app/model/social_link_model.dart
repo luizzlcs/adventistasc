68f71cbb08812748dd85ecfacc7f9eeb5686238e
Luiz Carlos <luizzlcs@gmail.com>
2025-01-29 21:30:58 -0300

    Clase modelo para os botões

---
+++ lib/app/model/social_link_model.dart
@@ -0,0 +1,14 @@
+
+class SocialLinkModel {
+  final String title;
+  final String url;
+  final String icon;
+  final String share;
+
+  const SocialLinkModel({
+    required this.title,
+    required this.url,
+    required this.icon,
+    required this.share,
+  });
+}
